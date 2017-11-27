598ef72edabbc4c849d96a91cff9320e5c6b3fff
Alex Boffey <aboffey@iweb.co.uk>
2017-11-27 20:00:16 +0000

    Move animation to content wrapper from page wrapper

--- src/layouts/content.jsx
+++ src/layouts/content.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Styled from 'styled-components'
+
+const Section = Styled.section`
+    animation: Fade 300ms ease;
+
+    @keyframes Fade {
+        0% { opacity: 0; }
+        100% { opacity: 1; }
+    }
+`
 
 const Content = ({ children, isFullWidth }) => {
     const columnsClass = isFullWidth ? 'g-col-xs-12' : 'g-col-sm-10 g-col-lg-9 g-col-xl-8'
 
     return (
-        <section className="content__main u-section u-fill-neutral-readable">
+        <Section className="content__main u-section u-fill-neutral-readable">
             <div className="u-container-lg">
                 <div className="u-section u-block grid">
                     <div className={columnsClass}>
@@ -13,7 +23,7 @@ const Content = ({ children, isFullWidth }) => {
                     </div>
                 </div>
             </div>
-        </section>
+        </Section>
     )
 }
 
--- src/layouts/wrapper.jsx
+++ src/layouts/wrapper.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
-import Styled from 'styled-components'
-
-const Section = Styled.section`
-    animation: Fade 300ms ease;
-
-    @keyframes Fade {
-        0% { opacity: 0; }
-        100% { opacity: 1; }
-    }
-`
 
 export default ({ children }) =>
-    <Section className="content__wrapper u-fill-neutral-white">
+    <section className="content__wrapper u-fill-neutral-white">
         {children}
-    </Section>
+    </section>
