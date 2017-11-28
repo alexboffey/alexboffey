ed30e4647d28d28c59ebacebbff241aab9d63532
Alex Boffey <aboffey@iweb.co.uk>
2017-11-28 20:00:30 +0000

    Layout changes

--- src/components/single-blog.jsx
+++ src/components/single-blog.jsx
@@ -6,16 +6,14 @@ const SingleBlog = ({ title, subtitle, date, excerpt, slug }) =>
     <article className="blog-single card u-section-sm-bottom u-block-xl">
         <header className="blog-single__header border-block border-block--alt">
             <h2 className="h1">{title}</h2>
-            <p>{subtitle}</p>
+            <div className="u-section-xxs-top">
+                <p className="micro">{date}</p>
+            </div>
         </header>
 
         <div className="blog-single__content u-section-xxs-top">
             <div className="grid">
                 <div className="g-col-sm-10">
-                    <header className="u-secion-xxs-bottom">
-                        <p className="micro">{date}</p>
-                    </header>
-                    
                     <p>{excerpt}</p>
                 </div>
             </div>
