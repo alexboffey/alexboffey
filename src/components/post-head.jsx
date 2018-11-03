d8558ee26410f4e2531aa5a57b488f6ca12a2062
Alex Boffey <alex@alexboffey.co.uk>
2018-11-03 20:24:45 +0000

    Fix cases for non-static proptypes errors

--- src/components/post-head.jsx
+++ src/components/post-head.jsx
@@ -2,35 +2,27 @@ import React, { Component } from "react";
 import Helmet from "react-helmet";
 import PropTypes from "prop-types";
 
-export default class PostHead extends Component {
-  propTypes = {
-    data: PropTypes.object.isRequired,
-    post: PropTypes.object.isRequired,
-    defaultTags: PropTypes.string
-  };
+const PostHead = ({ data, post, defaultTags }) => (
+  <Helmet title={`${data.site.siteMetadata.title} | ${post.frontmatter.title}`}>
+    <meta
+      name="description"
+      content={`${post.frontmatter.title}, ${data.site.siteMetadata.title}.`}
+    />
+    <meta
+      name="tags"
+      content={
+        post.frontmatter.tags
+          ? post.frontmatter.tags + `,${defaultTags}`
+          : defaultTags
+      }
+    />
+  </Helmet>
+);
 
-  render() {
-    const { data, post, defaultTags } = this.props;
+PostHead.propTypes = {
+  data: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
+  defaultTags: PropTypes.string
+};
 
-    return (
-      <Helmet
-        title={`${data.site.siteMetadata.title} | ${post.frontmatter.title}`}
-      >
-        <meta
-          name="description"
-          content={`${post.frontmatter.title}, ${
-            data.site.siteMetadata.title
-          }.`}
-        />
-        <meta
-          name="tags"
-          content={
-            post.frontmatter.tags
-              ? post.frontmatter.tags + `,${defaultTags}`
-              : defaultTags
-          }
-        />
-      </Helmet>
-    );
-  }
-}
+export default PostHead;
--- src/layouts/content.jsx
+++ src/layouts/content.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Content = ({ children, isFullWidth }) => {
-  const columnsClass = isFullWidth
-    ? "g-col-xs-12"
-    : "g-col-sm-10 g-col-lg-9 g-col-xl-8";
-
-  return (
-    <section className="content__main u-section u-fill-neutral-readable">
-      <div className="u-container-lg">
-        <div className="u-section u-block grid">
-          <div className={columnsClass}>{children}</div>
+const Content = ({ children, isFullWidth }) => (
+  <section className="content__main u-section u-fill-neutral-readable">
+    <div className="u-container-lg">
+      <div className="u-section u-block grid">
+        <div
+          className={
+            isFullWidth ? "g-col-xs-12" : "g-col-sm-10 g-col-lg-9 g-col-xl-8"
+          }
+        >
+          {children}
         </div>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 Content.propTypes = {
   isFullWidth: PropTypes.bool
