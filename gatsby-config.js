892a8a6d82f704865fae5748c59b1cc009070474
Alex Boffey <alex@alexboffey.co.uk>
2018-11-04 21:28:14 +0000

    Add gatsby-plugin-styled-components & update NPM dependencies.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
